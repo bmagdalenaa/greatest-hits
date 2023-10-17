ab2232e1cf89a3cc652b993b52a59ea663a77fd9
Leonardo Lourencini <leolourencini@hotmail.com>
2023-10-17 14:54:21 -0500

    Added validation to Album table

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,7 @@ class Album < ApplicationRecord
   belongs_to :artist
   has_many :lyrics # ??
   has_many :songs # ??
+
+  validates :album_name, presence: true
+  validates :album_name, uniqueness: true
 end
