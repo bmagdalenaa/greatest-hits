7d5ed551c30a57b0b51b5fcf528133071a0c19a5
Leonardo Lourencini <leolourencini@hotmail.com>
2023-10-18 09:59:59 -0500

    Added search function to artists controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,4 +8,10 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
     @album = Album.all.where("artist_id" == @artist)
   end
+
+  def search
+    wildcard_search = "%#{params[:keywords]}%"
+    @artists = Artist.where("artist_name LIKE ?", wildcard_search)
+    @albuns = Album.where("album_name LIKE ?", wildcard_search)
+  end
 end
